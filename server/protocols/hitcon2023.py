300bb2b019a28e5d707875dc1be0f2df26e873a6
bogay <pojay11523@gmail.com>
2023-11-15 09:05:29 +0800

    feat(protocols/hitcon2023): put to queue when pending / limit exceeded

--- server/protocols/hitcon2023.py
+++ server/protocols/hitcon2023.py
@@ -6,6 +6,9 @@ from server import app
 from server.models import Flag, SubmitResult, FlagStatus
 
 
+put_to_queue = {"reject flag submission at round pending", "Limit exceeded"}
+
+
 def submit_flags(flags: List[Flag], config: Dict[str, Any]):
     system_url = config["SYSTEM_URL"]
     token = config["SYSTEM_TOKEN"]
@@ -24,5 +27,12 @@ def submit_flags(flags: List[Flag], config: Dict[str, Any]):
             status = FlagStatus.ACCEPTED
         elif 400 <= resp.status_code <= 499:
             status = FlagStatus.REJECTED
+            try:
+                rjson = resp.json()
+                err = rjson["error"]
+                if err in put_to_queue:
+                    status = FlagStatus.QUEUED
+            except Exception as e:
+                app.logger.warning(f"failed to parse check system response: {e}")
 
         yield SubmitResult(flag.flag, status, resp.text)
